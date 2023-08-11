eb0dc3f07161c5444af4e9aa7daf849b9ab4a9c3
Pranjul Chaurasiya <pranjul.19641@knit.ac.in>
2023-08-11 21:28:58 +0530

    make_equal_st

--- Array/make_equal.cpp
+++ Array/make_equal.cpp
@@ -1,3 +1,33 @@
+
+
+
+
+
+
+// Given an array of all positive integers and an element “x”. 
+
+// You need to find out whether all array elements can be made 
+// equal or not by performing any of the 3 operations: add x to any 
+// element in array, subtract x from any element from array, do nothing.
+
+//  This operation can be performed only once on an element of array.
+
+
+
+// Input 1:
+// A=[2,3,1]
+// X=1
+// Input 2:
+
+// A=[2,3,1]
+// X=2
+
+
+
+
+
+
+
 int Solution::solve(vector<int> &A, int B) {
     
     

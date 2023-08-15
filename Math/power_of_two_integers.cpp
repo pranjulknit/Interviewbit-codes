60ec2456c97aba44bf2ca6fcdc19e0f2e85e68ab
Pranjul Chaurasiya <pranjul.19641@knit.ac.in>
2023-08-15 21:19:35 +0530

    power_of_integers

---
+++ Math/power_of_two_integers.cpp
@@ -0,0 +1,31 @@
+// Problem Description
+ 
+ 
+
+// Given a positive integer that fits in a 32-bit signed integer, 
+// find if it can be expressed as A^P where P > 1 and A > 0.
+// A and P both should be integers.
+
+
+// Problem Constraints
+// 1 <= A <= 109
+
+
+// Input Format
+// The first argument is an integer A.
+
+
+// Output Format
+// Return an integer, 1 for True, 0 for False
+
+
+// Example Input
+// A = 4
+
+
+// Example Output
+// 1
+
+
+// Example Explanation
+// A = 4, it can be written as 2 ^ 2
